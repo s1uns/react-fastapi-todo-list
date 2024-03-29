84b765dcdb6a43163b7eb4755313353fd24185dc
Illia Teliuk <illiateliuk@gmail.com>
2024-03-29 23:45:16 +0200

    FInished the ToDoList component

--- app/components/ToDoList.tsx
+++ app/components/ToDoList.tsx
@@ -1,25 +1,37 @@
 "use client";
 
 import React, { useState } from "react";
-import { ToDoItem, ToDoObject } from "@/components/ToDoItem";
+import { ToDoItem } from "@/components/ToDoItem";
+import { ToDoObject } from "@/types/todo";
 
 export const ToDoList: React.FC = () => {
     const [todos, setTodos] = useState<ToDoObject[]>([
         { id: "1", name: "task 1", priority: 5, isDone: true },
-        { id: "2", name: "task 2", priority: 3, isDone: false },
+        { id: "2", name: "task 2", priority: 10, isDone: false },
     ]);
 
     return (
         <div>
-            {todos.map((item) => (
-                <ToDoItem
-                    key={item.id}
-                    id={item.id}
-                    name={item.name}
-                    priority={item.priority}
-                    isDone={item.isDone}
-                />
-            ))}
+            <table className="shadow-lg bg-white border-separate w-full text-5xl">
+                <tbody>
+                    <tr>
+                        <th className="bg-blue-100 border px-8 py-4">Title</th>
+                        <th className="bg-blue-100 border px-8 py-4">
+                            Priority
+                        </th>
+                        <th className="border text-left px-8 py-4"></th>
+                    </tr>
+                    {todos.map((item) => (
+                        <ToDoItem
+                            key={item.id}
+                            id={item.id}
+                            name={item.name}
+                            priority={item.priority}
+                            isDone={item.isDone}
+                        />
+                    ))}{" "}
+                </tbody>
+            </table>
         </div>
     );
 };
