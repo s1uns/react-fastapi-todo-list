43e9fa546ebff44310e87dc68c4b38d27729eefc
Illia Teliuk <illiateliuk@gmail.com>
2024-03-29 23:43:35 +0200

    Updated todo item

--- app/components/ToDoItem.tsx
+++ app/components/ToDoItem.tsx
@@ -1,16 +1,58 @@
+import { ToDoObject } from "@/types/todo";
 import React from "react";
+import { Button } from "@/components/Button";
+import PriorityBar from "@/components/PriorityBar";
 
-export type ToDoObject = {
-    id: string;
-    name: string;
-    priority: number;
-    isDone: boolean;
-};
-
-export const ToDoItem: React.FC<ToDoObject> = ({id, name, priority, isDone }) => {
+export const ToDoItem: React.FC<ToDoObject> = ({
+    id,
+    name,
+    priority,
+    isDone,
+}) => {
     return (
-        <div>
-            {name} {priority} {isDone ? "✅" : "❌"}
-        </div>
+        <tr
+            className={
+                isDone
+                    ? `relative opacity-50 after:absolute after:px-5 after:left-[1%] after:top-2/4 after:h-px after:bg-black after:content-[""] after:w-[80%] `
+                    : ""
+            }
+        >
+            <td className="border px-8 py-4 ">{name}</td>
+            <td className="border px-8 py-4">
+                <PriorityBar priority={priority} />
+            </td>
+            <td className="border px-8 py-4">
+                <div className="flex flex-row gap-5 justify-end">
+                    <Button
+                        handleClick={() => {}}
+                        styles={"h-full "}
+                        type={"button"}
+                        title={"EDIT TASK"}
+                        disabled={isDone}
+                    >
+                        EDIT TASK
+                    </Button>
+                    <Button
+                        handleClick={() => {}}
+                        styles={`h-full`}
+                        type={"button"}
+                        title={"DELETE TASK"}
+                        disabled={isDone}
+                    >
+                        DELETE TASK{" "}
+                    </Button>
+                    <Button
+                        handleClick={() => {}}
+                        styles={`h-full w-1/4  `}
+                        type={"button"}
+                        title={"MARK AS DONE/UNDONE"}
+                        disabled={false}
+                    >
+                        MARK AS {isDone ? "UNDONE" : "DONE"}
+                    </Button>
+                </div>
+            </td>
+        </tr>
+        // {/* {name} {priority} {isDone ? "✅" : "❌"} */}
     );
 };
