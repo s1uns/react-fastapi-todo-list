fbf3cadeac57c86179910443e245102250d92440
Illia Teliuk <illiateliuk@gmail.com>
2024-03-30 20:52:37 +0200

    Added modal pop-ups

--- app/components/ControlPanel.tsx
+++ app/components/ControlPanel.tsx
@@ -1,33 +1,97 @@
 "use client";
 
-import React from "react";
+import React, { FormEventHandler, useState } from "react";
 import { Button } from "@/components/Button";
 import { AiOutlinePlus } from "react-icons/ai";
 import { Search } from "@/components/Search";
-import { TbFilterX } from "react-icons/tb";
+import { Modal } from "@/components/Modal";
+import { Slider } from "@/components/Slider";
 
 export const ControlPanel: React.FC = () => {
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+    const [newTaskValue, setNewTaskValue] = useState<string>("");
+    const [newPriorityValue, setNewPriorityValue] = useState<number>(10);
+    const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = () => {};
+
+    // const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (
+    //     e
+    // ) => {
+    //     e.preventDefault();
+    //     await addTodo({
+    //         id: uuidv4(),
+    //         text: newTaskValue,
+    //     });
+    //     setNewTaskValue("");
+    //     setModalOpen(false);
+    //     router.refresh();
+    // };
+
+    const updateSlider = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewPriorityValue((priority) => 11 - +e.target.value);
+    };
     return (
-        <div className="flex flex-row gap-16 px-20 w-full h-40 text-cyan-200 justify-center">
-            <Button
-                handleClick={() => {}}
-                styles={"h-full w-1/4  "}
-                type={"button"}
-                title={"ADD TASK"}
-                disabled={false}
-            >
-                ADD TASK
-                <AiOutlinePlus className="ml-2" size={24} />
-            </Button>
+        <div className="flex flex-row gap-16 px-20 w-full h-80  justify-center bg-gray-300 py-8 rounded">
+            <div className="w-1/4">
+                <Button
+                    handleClick={() => {
+                        setNewTaskValue("");
+                        setNewPriorityValue(10);
+                        setModalOpen(true);
+                    }}
+                    styles={"h-1/2 w-full  "}
+                    type={"button"}
+                    title={"ADD TASK"}
+                    disabled={false}
+                >
+                    ADD TASK
+                    <AiOutlinePlus className="ml-2" size={24} />
+                </Button>
+                <Modal
+                    modalOpen={modalOpen}
+                    setModalOpen={setModalOpen}
+                    title={"Add new task"}
+                >
+                    <form onSubmit={() => {}}>
+                        <div className="modal-action">
+                            <input
+                                value={newTaskValue}
+                                onChange={(e) =>
+                                    setNewTaskValue(e.target.value)
+                                }
+                                type="text"
+                                placeholder="Enter task title"
+                                className="input input-bordered w-full"
+                            />
+
+                            <Slider
+                                title="Select tasks' priority"
+                                defaultValue={newPriorityValue}
+                                minValue={1}
+                                maxValue={10}
+                                step={1}
+                                onSlide={updateSlider}
+                            />
+                            <Button
+                                type="submit"
+                                styles="btn hover:translate-y-0 absolute bottom-3 right-2 w-1/3 h-1/4"
+                                title={"CREATE TASK"}
+                                disabled={false}
+                            >
+                                Create
+                            </Button>
+                        </div>
+                    </form>
+                </Modal>
+            </div>
             <div className="flex flex-row w-1/3 gap-6 items-end justify-center h-full px-3">
                 <div className="flex flex-col w-full h-full justify-start">
-                    <h3 className="text-center text-6xl h-full">
+                    <h3 className="text-center flex flex-col justify-end text-6xl h-full text-cyan-500	">
                         Sort by priority:
                     </h3>
                     <div className="flex flex-row justify-between items-end h-full w-full gap-5">
                         <Button
                             handleClick={() => {}}
-                            styles={"h-full w-1/2"}
+                            styles={"h-2/3 w-1/2"}
                             type={"button"}
                             title={"ASC SORT"}
                             disabled={false}
@@ -36,7 +100,7 @@ export const ControlPanel: React.FC = () => {
                         </Button>
                         <Button
                             handleClick={() => {}}
-                            styles={"h-full w-1/2"}
+                            styles={"h-2/3 w-1/2"}
                             type={"button"}
                             title={"DESC SORT"}
                             disabled={false}
@@ -45,7 +109,7 @@ export const ControlPanel: React.FC = () => {
                         </Button>
                     </div>
                 </div>
-                <Button
+                {/* <Button
                     handleClick={() => {}}
                     styles={"h-full w-1/2"}
                     type={"button"}
@@ -53,7 +117,7 @@ export const ControlPanel: React.FC = () => {
                     disabled={false}
                 >
                     <TbFilterX size={50} />
-                </Button>
+                </Button> */}
             </div>
             <Search />
         </div>
--- app/components/ToDoItem.tsx
+++ app/components/ToDoItem.tsx
@@ -1,7 +1,9 @@
 import { ToDoObject } from "@/types/todo";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/Button";
 import PriorityBar from "@/components/PriorityBar";
+import { Modal } from "@/components/Modal";
+import { Slider } from "@/components/Slider";
 
 export const ToDoItem: React.FC<ToDoObject> = ({
     id,
@@ -9,32 +11,83 @@ export const ToDoItem: React.FC<ToDoObject> = ({
     priority,
     isDone,
 }) => {
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+    const [editTaskValue, setEditTaskValue] = useState<string>(name);
+    const [editPriorityValue, setEditPriorityValue] =
+        useState<number>(priority);
+
+    const updateSlider = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEditPriorityValue((oldPriority) => 11 - +e.target.value);
+    };
+
     return (
-        <tr
-            className={
-                isDone
-                    ? `relative opacity-50 after:absolute after:px-5 after:left-[1%] after:top-2/4 after:h-px after:bg-black after:content-[""] after:w-[80%] `
-                    : ""
-            }
-        >
-            <td className="border px-8 py-4 ">{name}</td>
+        <tr className={` text-white ${isDone ? `bg-slate-200 ` : ""}`}>
+            <td
+                className={`text-black border px-8 py-4 ${
+                    isDone ? `opacity-50 ` : ""
+                }`}
+            >
+                {name}
+            </td>
             <td className="border px-8 py-4">
                 <PriorityBar priority={priority} isDone={isDone} />
             </td>
             <td className="border px-8 py-4">
                 <div className="flex flex-row gap-5 justify-end">
+                    <div className="w-1/4 flex justify-end">
+                        <Button
+                            handleClick={() => {
+                                setEditTaskValue(name);
+                                setEditPriorityValue(priority);
+                                setModalOpen(true);
+                            }}
+                            styles={"h-full w-full"}
+                            type={"button"}
+                            title={"EDIT TASK"}
+                            disabled={isDone}
+                        >
+                            EDIT TASK
+                        </Button>
+                        <Modal
+                            modalOpen={modalOpen}
+                            setModalOpen={setModalOpen}
+                            title={"Update task's info"}
+                        >
+                            <form onSubmit={() => {}}>
+                                <div className="modal-action">
+                                    <input
+                                        value={editTaskValue}
+                                        onChange={(e) =>
+                                            setEditTaskValue(e.target.value)
+                                        }
+                                        type="text"
+                                        placeholder="Enter new task's title"
+                                        className="input input-bordered w-full"
+                                    />
+
+                                    <Slider
+                                        title="Select tasks' priority"
+                                        defaultValue={priority}
+                                        minValue={1}
+                                        maxValue={10}
+                                        step={1}
+                                        onSlide={updateSlider}
+                                    />
+                                    <Button
+                                        type="submit"
+                                        styles="btn hover:translate-y-0 absolute bottom-3 right-2 w-1/3 h-1/4"
+                                        title={"CREATE TASK"}
+                                        disabled={false}
+                                    >
+                                        Update
+                                    </Button>
+                                </div>
+                            </form>
+                        </Modal>
+                    </div>
                     <Button
                         handleClick={() => {}}
-                        styles={"h-full "}
-                        type={"button"}
-                        title={"EDIT TASK"}
-                        disabled={isDone}
-                    >
-                        EDIT TASK
-                    </Button>
-                    <Button
-                        handleClick={() => {}}
-                        styles={`h-full`}
+                        styles={`h-full w-1/4`}
                         type={"button"}
                         title={"DELETE TASK"}
                         disabled={isDone}
@@ -43,7 +96,7 @@ export const ToDoItem: React.FC<ToDoObject> = ({
                     </Button>
                     <Button
                         handleClick={() => {}}
-                        styles={`h-full w-1/4  `}
+                        styles={`h-full w-1/4 opacity-1  `}
                         type={"button"}
                         title={"MARK AS DONE/UNDONE"}
                         disabled={false}
