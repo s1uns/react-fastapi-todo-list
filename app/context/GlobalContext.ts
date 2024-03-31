15d1e0ddaecafd7277d9d835f600c2c5cb0303ae
Illia Teliuk <illiateliuk@gmail.com>
2024-03-31 14:33:47 +0300

    Added new method to context to easily fetch and set todos

--- app/context/GlobalContext.ts
+++ app/context/GlobalContext.ts
@@ -1,3 +1,4 @@
+import { fetchTodos } from "@/services/api";
 import { ToDoObject } from "@/types/todo";
 import { createContext, useContext } from "react";
 export type GlobalContent = {
@@ -5,20 +6,39 @@ export type GlobalContent = {
     setTodos: (c: ToDoObject[]) => void;
     currentCompletness: "all" | "done" | "undone";
     setCurrentCompletness: (cC: "all" | "done" | "undone") => void;
-    searchQuery: string;
-    setSearchQuery: (sQ: string) => void;
+    searchString: string;
+    setSearchString: (sQ: string) => void;
     order: "none" | "asc" | "desc";
     setOrder: (o: "none" | "asc" | "desc") => void;
+    updateTodos: (
+        currentCompletness: string,
+        searchString: string,
+        order: string,
+        setTodos: (c: ToDoObject[]) => void
+    ) => void;
 };
 export const GlobalContext = createContext<GlobalContent>({
     todos: [],
     setTodos: () => {},
     currentCompletness: "all",
     setCurrentCompletness: () => {},
-    searchQuery: "",
-    setSearchQuery: () => {},
+    searchString: "",
+    setSearchString: () => {},
     order: "none",
     setOrder: () => {},
+    updateTodos: async (
+        currentCompletness: string,
+        searchString: string,
+        order: string,
+        setTodos: (c: ToDoObject[]) => void
+    ) => {
+        const response = await fetchTodos(
+            currentCompletness,
+            searchString,
+            order
+        );
+        setTodos(response);
+    },
 });
 
 export const useGlobalContext = () => useContext(GlobalContext);
