fba6435434e4ca1cdbaabd5c88e1eca37d6e71aa
Illia Teliuk <illiateliuk@gmail.com>
2024-04-01 19:15:29 +0300

    Updated themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages//*.{js,ts,jsx,tsx,mdx}",
+        "./components//*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
@@ -33,7 +33,7 @@ const config: Config = {
     },
     plugins: [require("daisyui")],
     daisyui: {
-        themes: ['light'],
-      }
+        themes: false,
+    },
 };
 export default config;
